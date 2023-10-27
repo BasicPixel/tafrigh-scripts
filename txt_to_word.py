549db90883ecdcb1a35564f5eb381948288463cd
Pixel <nightknight218@gmail.com>
2023-10-27 17:47:19 +0300

    Fix error that happens when opening binary files

--- txt_to_word.py
+++ txt_to_word.py
@@ -9,13 +9,19 @@ if not os.path.exists("./output"):
     os.makedirs("./output")
 
 for file in file_list:
+
     filename = os.path.splitext(file)[0]
     document = Document()
     document.add_heading(filename, 0)
 
     # Get file contents
     # If on Windows, specify encoding="utf-8" for open function
-    file_contents = open(path + file).read()
+    try:
+        file_contents = open(path + file).read()
+
+    # Break loop if file is binary (non-text)
+    except UnicodeDecodeError:
+        break
 
     # Remove unwanted words
     final_text = file_contents.replace(" آآ ", " ").replace(
